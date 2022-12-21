bb8949694b3e70488b8245d7ffaf7fbe87c3ca73
Che-tsun Ho <republicho@foxmail.com>
2022-12-21 13:40:10 +0800

    update

---
+++ src/main/java/com/hybridsearch/searcher/GenericImageSearcher.java
@@ -0,0 +1,15 @@
+package com.hybridsearch.searcher;
+
+import java.util.logging.Logger;
+
+/**
+ * @Author Anthony Z.
+ * @Date 21/12/2022
+ * @Description:
+ */
+public class GenericImageSearcher {
+
+    protected Logger logger = Logger.getLogger(getClass().getName());
+    protected String fieldName;
+
+}
